020515e04ed250f836f3e8e37b5a10526155d5ed
mahadev4995 <84270273+mahadev4995@users.noreply.github.com>
2023-07-03 14:36:14 +0530

    first

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-     <img src="./src/assets/logo.svg" alt="logo" />
+     <img src="./public/assets/logo.svg" alt="logo" />
     </header>
   );
 };
